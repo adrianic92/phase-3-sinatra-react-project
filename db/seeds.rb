d7e731b9fd6efe4dbfc5b442690590457a70acca
Adrian <adrianic92@yahoo.com>
2023-03-01 18:34:52 -0800

    added restaurant info to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,11 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+10.times do |i|
+    Restaurant.create(name: "#{Faker::Restaurant.unique.name}", description: "#{Faker::Restaurant.unique.description}", rating: rand(5..10), location: "#{Faker::Address.unique.full_address}", id: i+1)
+end
+
+
+
+
 puts "✅ Done seeding!"
