eb875ae6235ffa9c5e3930ead8548c467dc47d07
Adrian <adrianic92@yahoo.com>
2023-03-01 20:26:06 -0800

    updated Restaurant methods

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,5 +5,11 @@ class Restaurant < ActiveRecord::Base
         #returns an array of all food names that the restaurant serves
         self.foods.map { |t| t.food_name}
     end
+    
+    def self.ten_out_of_ten
+        #returns the name of the first record with a 10 rating. If no record has a 10 rating, returns message.
+        michellin = self.find_by(rating: 10)
+        michellin ? michellin : "There are no perfect restaurants out there..."
+    end
 
 end
