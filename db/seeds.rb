6d916cb72859063fe55961ee90a6ceddaa893261
Adrian <adrianic92@yahoo.com>
2023-03-01 18:43:05 -0800

    added food info to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@ puts "🌱 Seeding spices..."
     Restaurant.create(name: "#{Faker::Restaurant.unique.name}", description: "#{Faker::Restaurant.unique.description}", rating: rand(5..10), location: "#{Faker::Address.unique.full_address}", id: i+1)
 end
 
+50.times do
+    Food.create(food_name: "#{Faker::Food.unique.name}", dessert_pairing: "#{Faker::Dessert.flavor} #{Faker::Dessert.variety} with #{Faker::Dessert.topping}", restaurant_id: rand(1..10))
+end
+
 
 
 
