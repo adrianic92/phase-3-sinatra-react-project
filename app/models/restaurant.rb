4b983b1a4f3672ee682dfd3fe00c721faf3d7240
Adrian <adrianic92@yahoo.com>
2023-03-02 20:36:58 -0800

    updated top method

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,7 +8,7 @@ class Restaurant < ActiveRecord::Base
     
     def self.ten_out_of_ten
         #returns the name of the first record with a 10 rating. If no record has a 10 rating, returns message.
-        michellin = self.find_by(rating: 10)
+        michellin = self.find_by(rating: 10).name
         michellin ? michellin : "There are no perfect restaurants out there..."
     end
 
