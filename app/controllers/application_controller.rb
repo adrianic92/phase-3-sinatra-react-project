1f4d540b65875d1791047924407f26fa2ff7f9a1
Adrian <adrianic92@yahoo.com>
2023-03-02 20:18:47 -0800

    added routes to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,44 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/restaurants' do
+    restaurants = Restaurant.all
+    restaurants.to_json
   end
 
+  post '/restaurants' do
+    restaurant = Restaurant.create(
+      name: params[:name],
+      description: params[:description],
+      rating: params[:rating],
+      location: params[:location]
+    )
+  end
+
+  get '/foods' do
+    foods = Food.all
+    foods.to_json
+  end
+
+  post '/foods' do
+    food = Food.create(
+      food_name: params[:food_name],
+      dessert_pairing: params[:dessert_pairing],
+      restaurant_id: params[:restaurant_id]
+    )
+  end
+
+  patch '/foods/:id' do
+    food = Food.find(params[:id])
+    food = update(
+      food_name: params[:food_name],
+      dessert_pairing: params[:dessert_pairing]
+    )
+  end
+
+  delete '/foods/:id' do
+    food = Food.find(params[:id])
+    food.destroy
+    food.to_json
+  end
 end
