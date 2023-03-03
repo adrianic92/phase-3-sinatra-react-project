822de5c75fd97d5fed7463b2b1c65e96f3bda57e
Adrian <adrianic92@yahoo.com>
2023-03-02 21:11:00 -0800

    removed extra method

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,5 @@
 class Restaurant < ActiveRecord::Base
     has_many :foods
-
-    def my_foods
-        #returns an array of all food names that the restaurant serves
-        self.foods.map { |t| t.food_name}
-    end
     
     def self.ten_out_of_ten
         #returns the name of the first record with a 10 rating. If no record has a 10 rating, returns message.
